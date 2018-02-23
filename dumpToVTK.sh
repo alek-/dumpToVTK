6a8bf2b2a6956a65e03985bebbf4d95fc444062f
alek- <alek-@users.noreply.github.com>
2018-02-23 16:59:07 -0300

    Update dumpToVTK.sh

--- dumpToVTK.sh
+++ dumpToVTK.sh
@@ -23,48 +23,48 @@ printf "ASCII\n" >> $datafile
 printf "DATASET POLYDATA\n" >> $datafile
 
 # Dump data structure
-#  1  2    3   4 5 6  7  8  9 10 11 12 13 14 15   16     17     18     19
-# id type type x y z ix iy iz vx vy vz fx fy fz omegax omegay omegaz radius 
+#  1  2   3 4 5  6  7  8  9 10 11 12 13 14   15    16     17     18
+# id type x y z ix iy iz vx vy vz fx fy fz omegax omegay omegaz radius 
 
 #coordinates
 printf "POINTS $atoms float\n" >> $datafile
-#echo "Writing coordinates"
 
-awk '{print $4, $5, $6}' $tmpfile >> $datafile
+#echo "Writing coordinates"
+awk '{print $3, $4, $5}' $tmpfile >> $datafile
 
 #echo "Vertices"
 let atoms2=$atoms*2
 printf "VERTICES $atoms $atoms2\n" >> $datafile
-awk -v var="$atoms" '{print $2, NR-1}' $tmpfile >> $datafile
+awk '{print $2, NR-1}' $tmpfile >> $datafile
 
 #echo "Point data"
 printf "POINT_DATA $atoms\n" >> $datafile
 printf "VECTORS i float\n" >> $datafile
-awk '{print $7, $8, $9}' $tmpfile >> $datafile
+awk '{print $6, $7, $8}' $tmpfile >> $datafile
 
 #echo "Forces"
 printf "VECTORS f float\n" >> $datafile
-awk '{print $13, $14, $15}' $tmpfile >> $datafile
+awk '{print $12, $13, $14}' $tmpfile >> $datafile
 
 #echo "Omega"
 printf "VECTORS omega float\n" >> $datafile
-awk '{print $16, $17, $18}' $tmpfile >> $datafile
+awk '{print $15, $16, $17}' $tmpfile >> $datafile
 
 #echo "Velocities"
 printf "VECTORS v float\n" >> $datafile
-awk '{print $10, $11, $12}' $tmpfile >> $datafile
+awk '{print $9, $10, $11}' $tmpfile >> $datafile
 
 #echo "Radius"
 printf "SCALARS radius float 1\n" >> $datafile
 printf "LOOKUP_TABLE default\n" >> $datafile
-awk '{print $19}' $tmpfile >> $datafile
+awk '{print $18}' $tmpfile >> $datafile
 
 #echo "Type"
 printf "SCALARS type float 1\n" >> $datafile
 printf "LOOKUP_TABLE default\n" >> $datafile
 awk '{print $2}' $tmpfile >> $datafile
 
-#echo "Type"
+#echo "id"
 printf "SCALARS id float 1\n" >> $datafile
 printf "LOOKUP_TABLE default\n" >> $datafile
 awk '{print $1}' $tmpfile >> $datafile
